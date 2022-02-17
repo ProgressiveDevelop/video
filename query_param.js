3121dd4020ade1a6e8556d0931982a5db1e64cb9
WebProgress <48899616+ProgressiveDevelop@users.noreply.github.com>
2022-02-18 07:18:45 +0800

    Add files via upload

---
+++ query_param.js
@@ -0,0 +1,5 @@
+ function queryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        return r === null ? null : decodeURIComponent(r[2]);
+    }
